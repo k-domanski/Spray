29d486453bcebde5538a34be4eb9473b3880bc11
Ignole <ignole@icloud.com>
2021-03-25 00:43:17 +0100

    Added sound to AK47

--- Spray/Assets/Scripts/weapons/GunController.cs
+++ Spray/Assets/Scripts/weapons/GunController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,23 @@ public class GunController : MonoBehaviour
 {
     [SerializeField] private WeaponStats _weaponStats;
     [SerializeField] private Transform _muzzlePoint;
+    [SerializeField] private AudioSource _shotAudio;
     private bool _canShoot = true;
+
+    private void Start()
+    {
+        _shotAudio = GetComponent<AudioSource>();
+    }
 
     public void Shoot(Vector3 aimDirection)
     {
         if (_canShoot)
         {
             _canShoot = false;
-
+            _shotAudio.Play();
             _weaponStats.CreateProjectile(_muzzlePoint.position, aimDirection);
             this.Delay(() => { _canShoot = true; }, 1f / _weaponStats.fireRate);
         }
+        
     }
 }
