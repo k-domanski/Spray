756331eced28e22279bf1ba925449d3f4b90a662
Ignole <ignole@icloud.com>
2021-03-27 17:43:41 +0100

    Added Particle System (but it's not working :) ) along with it's prefab

--- Spray/Assets/Scripts/weapons/GunController.cs
+++ Spray/Assets/Scripts/weapons/GunController.cs
@@ -8,6 +8,7 @@ public class GunController : MonoBehaviour
     [SerializeField] private WeaponStats _weaponStats;
     [SerializeField] private Transform _muzzlePoint;
     [SerializeField] private AudioSource _shotAudio;
+    [SerializeField] private ParticleSystem _muzzleFlash;
     private bool _canShoot = true;
 
     private void Start()
@@ -21,6 +22,17 @@ public class GunController : MonoBehaviour
         {
             _canShoot = false;
             _shotAudio.Play();
+           
+           if (_muzzleFlash.isPlaying)
+            {
+                Debug.Log("Flash");
+                //_muzzleFlash.Stop();
+            }
+            _muzzleFlash.Emit(new ParticleSystem.EmitParams(), 10);
+            _muzzleFlash.gameObject.SetActive(true);
+            Debug.Log(_muzzleFlash.isEmitting);
+            _muzzleFlash.gameObject.SetActive(false);
+            
             _weaponStats.CreateProjectile(_muzzlePoint.position, aimDirection);
             this.Delay(() => { _canShoot = true; }, 1f / _weaponStats.fireRate);
         }
