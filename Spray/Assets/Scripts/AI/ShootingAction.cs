24b0510a413f10c102da7674282ee0ec1516316d
LinearJester <linearjester@gmail.com>
2021-04-24 17:28:36 +0200

    some balance testing and laser color change

--- Spray/Assets/Scripts/AI/ShootingAction.cs
+++ Spray/Assets/Scripts/AI/ShootingAction.cs
@@ -8,6 +8,6 @@ public class ShootingAction : Action
         Vector3 pos = enemy.target.transform.position;
         pos.y = enemy.transform.position.y;
         enemy.transform.LookAt(pos);
-        enemy.gunController.Shoot(enemy.transform.forward, 1f);
+        enemy.gunController.Shoot(enemy.transform.forward, 1f + (enemy.velocity.magnitude / enemy.settings.maxSpeed));
     }
 }
